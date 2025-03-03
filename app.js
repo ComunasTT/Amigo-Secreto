87236a0ea9d8828bd722b94d4cc4df774407a352
ComunasTT <gabrielantonio824@gmail.com>
2025-03-03 20:01:19 -0300

    Implementação de uma função para adicionar amigos

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let listaDeNomes = [];
+let totalAmigos = 0;
+
+function addAmigo(){
+    let nome = document.querySelector('#amigo').value;
+    
+    if(nome == ""){
+        alert("Por favor, insira um nome.");
+
+    } else if(listaDeNomes.includes(nome)){
+        alert('Esse nome já foi adicionado');
+    }
+    else{
+    listaDeNomes.push(nome);
+    totalAmigos++;
+    }
+    
+console.log(listaDeNomes);
+}
+
+console.log(totalAmigos);
