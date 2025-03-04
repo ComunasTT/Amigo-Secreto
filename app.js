2ec19b8e091091e1e326dcba5a98a44d30f8d7d2
ComunasTT <gabrielantonio824@gmail.com>
2025-03-04 20:37:59 -0300

    Adicionado a função para sorteio.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
 let listaDeNomes = [];
+let sorteado = [];
 
 //Essa função adiciona o nome dentro da array e já verifica se o nome é repetido e se o campo está vazio.
 
@@ -26,19 +27,35 @@ function adicionaAmigo(){
 console.log(listaDeNomes);
 }
 
+//Essa função limpa o campo após adicionar um campo.
+
 function limparCampo() {
     nome = document.querySelector('input');
     nome.value = '';
 }
 
+// Essa função vai montando a lista de nomes que foram adicionados para o sorteio.
 function montarLista(){
     let lista = document.getElementById('listaAmigos');
     for(i=0; i < listaDeNomes.length; i++){
         lista.innerHTML += `<li>${listaDeNomes[i]}</li>`;
     }
 }
-
+// Função para deixar a lista bonitinha após novo nome adicionado.
 function limpaLista(){
     document.getElementById('listaAmigos').innerHTML="";
     document.getElementById('resultado').innerHTML="";
+}
+
+// Função para sortear o seu amigo Secreto.
+function sorteioAmigos(){
+    if(listaDeNomes.length>0){
+    let numSorteado = parseInt(Math.random()*listaDeNomes.length);
+    sorteado = listaDeNomes[numSorteado];
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<li> Seu amigo secreto é o ${sorteado}.</li>`;
+    }
+    else {
+        adicionaAmigo();
+    }
 }
