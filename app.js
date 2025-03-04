130f5c2c2b330f60107db529065433f7c8bce04d
ComunasTT <gabrielantonio824@gmail.com>
2025-03-04 20:07:16 -0300

    Adicionado a função para limpar e montar a lista.

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
 let listaDeNomes = [];
-let totalAmigos = 0;
 
-function addAmigo(){
-    let nome = document.querySelector("input").value;
-    
+//Essa função adiciona o nome dentro da array e já verifica se o nome é repetido e se o campo está vazio.
+
+function adicionaAmigo(){
+    let nome = document.querySelector('input').value;
+    //Já coloca todo nome com primeira legra maiuscula.
+    nome = nome.substring(0,1).toUpperCase().concat(nome.substring(1));
+
     if(nome == ""){
         alert("Por favor, insira um nome.");
 
@@ -15,15 +18,27 @@ function addAmigo(){
     }
     else{
     listaDeNomes.push(nome);
-    totalAmigos++;
     limparCampo();
-    }
-    
+    limpaLista();
+    montarLista();
+
+    }    
 console.log(listaDeNomes);
-console.log(totalAmigos);
 }
 
 function limparCampo() {
     nome = document.querySelector('input');
     nome.value = '';
+}
+
+function montarLista(){
+    let lista = document.getElementById('listaAmigos');
+    for(i=0; i < listaDeNomes.length; i++){
+        lista.innerHTML += `<li>${listaDeNomes[i]}</li>`;
+    }
+}
+
+function limpaLista(){
+    document.getElementById('listaAmigos').innerHTML="";
+    document.getElementById('resultado').innerHTML="";
 }
